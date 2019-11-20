0e5d02e625c31fe86ec5862c220c6509710f4e7a
Ledwing Hernandez <ledwing.hernandez@gmail.com>
2019-11-20 16:29:09 -0500

    Following along with Laravel tutorial located at https://www.youtube.com/watch?v=ImtZ5yENzgE&t=1s Started make auth based on bootstrap Ran migration and edited to included enabled column with defaut value 1

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,8 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+	    $table->bigIncrements('id');
+	    $table->tinyInteger('enabled')->default(1);
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Led's Laravel Test Project
                 </div>
 
                 <div class="links">
